7dbca0c23f8b9c3c607b7494da224415b739507e
hcops <hcops@3vyd.com>
2019-06-10 09:45:50 +0800

    删除node-module模块及bug修复

--- day5/webpack-study/src/main.js
+++ day5/webpack-study/src/main.js
@@ -11,7 +11,7 @@ import './css/index.css'
 // 注意：webpack 默认只能打包处理 js 类型的文件，无法处理其它的非 JS 类型的文件
 // 如果要处理非js类型的文件，需要手动安装一些合适的第三方 loader 加载器
 // 1. 如果想要打包处理 css 文件，需要安装 npm i style-loader css-loader -D
-// 2。 打开webpack.config.js这个配置文件，在里面新增一个配置节点，叫做 module，它是一个对象，在这个 module 对象身上，有个rules属性，这个 rules属性是个数组，此数组中存放了所有第三方文件的匹配和处理规则
+// 2. 打开webpack.config.js这个配置文件，在里面新增一个配置节点，叫做 module，它是一个对象，在这个 module 对象身上，有个rules属性，这个 rules属性是个数组，此数组中存放了所有第三方文件的匹配和处理规则
 
 // 注意：webpack 处理第三方文件类型的过程
 // 1. 发现这个要处理的文件不是JS文件，然后就去配置文件中查找有没有对应的第三方 loader 规则
@@ -36,6 +36,6 @@ $(function() {
 // 1. 运行 npm i webpack-dev-server -D 把这个工具安装到项目的本地开发依赖
 // 2. 安装完毕后，这个工具的用法和 webpack 命令的用法完全一样
 // 3. 由于是在项目中安装的 webpack-dev-server，所以无法把它当作脚本命令，在终端直接运行（只有那些安装到全局 -g 的工具才能在终端执行）
-// 4. 注意：webpack-dev-server 这个工具，如果想要正常运行，要求在本地项目中必须安装 webpack
+// 4. 注意：webpack-dev-server 这个工具，如果想要正常运行，要求在本地项目中必须安装 webpack、webpack-cli
 // 5. webpack-dev-server 打包生成的bundle.js文件并没有存放到实际的物理磁盘上；而是直接托管到了电脑的内存中，所以，在项目的根目录中找不到这个打包好的 bundle.js
-// 6. 可以认为，webpack-dev-server 把打包好的文件以一种虚拟的形式托管到了项目的根目录中，虽然看不到它，但是可以认为和 dist src node_modules 平缓，有一个看不见的文件叫做 bundle.js
+// 6. 可以认为，webpack-dev-server 把打包好的文件以一种虚拟的形式托管到了项目的根目录中，虽然看不到它，但是可以认为和 dist src node_modules 平级，有一个看不见的文件叫做 bundle.js
